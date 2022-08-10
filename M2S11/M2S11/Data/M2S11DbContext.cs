811344598b33914c76b031bfe87bf04c123cc76b
Natanael Vieira <natanaelvieira130@gmail.com>
2022-08-10 11:45:28 -0300

    M2S11 - Exercicio 04

---
+++ M2S11/M2S11/Data/M2S11DbContext.cs
@@ -0,0 +1,20 @@
+﻿using M2S11.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace M2S11.Data {
+    public class M2S11DbContext : DbContext {
+
+        private IConfiguration _configuration;
+        public DbSet<Musica> Musicas { get; set; }
+        public DbSet<Album> Album { get; set; }
+        public DbSet<Artista> Artista { get; set; }
+
+        public M2S11DbContext(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("CONEXAO_BANCO"));
+        }
+    }
+}
